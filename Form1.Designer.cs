6dfca506fdf7eb5822ffe5980d90665415ff27b5
u0916721@umail.utah.edu <u0916721@umail.utah.edu>
2022-04-09 21:13:51 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace FishAuction
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.search = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // search
+      // 
+      this.search.Location = new System.Drawing.Point(591, 42);
+      this.search.Name = "search";
+      this.search.Size = new System.Drawing.Size(75, 23);
+      this.search.TabIndex = 0;
+      this.search.Text = "Find Fish";
+      this.search.UseVisualStyleBackColor = true;
+      this.search.Click += new System.EventHandler(this.search_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.search);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button search;
+  }
+}
+
